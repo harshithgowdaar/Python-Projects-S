b166d4546b51733c4b8fdbf5d873fc1bd27103a7
Sourabha V <sourabha2107@gmail.com>
2026-06-25 20:54:16 +0530

    add: modulus, floor division operations added

--- simple_calculator.py
+++ simple_calculator.py
@@ -9,18 +9,24 @@ def cal():
     print("1.Addition")
     print("2.Subtraction")
     print("3.Multiplication")
-    print("4.Division") 
-    choice=int(input("Enter choice (1/2/3/4):"))
+    print("4.Division")
+    print("5.Modulus")
+    print("6.Floor Division")
+    choice=int(input("Enter choice (1/2/3/4/5/6):"))
     print("-"*20)
         
     if choice==1:
-        print(f"sum of {a} and {b} is: {a+b}")
+        print(f"Sum of {a} and {b} is: {a+b}")
     elif choice==2:
-        print(f"difference of {a} and {b} is: {a-b}")
+        print(f"Difference of {a} and {b} is: {a-b}")
     elif choice==3:
-        print(f"product of {a} and {b} is: {a*b}")
+        print(f"Product of {a} and {b} is: {a*b}")
     elif choice==4:
-         print(f"division of {a} and {b} is: {a/b}")
+         print(f"Division of {a} and {b} is: {a/b}")
+    elif choice==5:
+         print(f"Modulus of {a} and {b} is: {a%b}")  
+    elif cjoice==6:    
+         print(f"Floor Division of {a} and {b} is: {a//b}")       
     else:
         print("invalid choice :(")
     print("-"*20)                   
